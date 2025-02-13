3495ce14fc8112447ad20c2d7c2393047b0adde7
omegamaster86 <masya868686@gmail.com>
2025-02-14 00:03:36 +0900

    update

---
+++ app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AppContainer from './app/components/app-container'
+import Navigator from './app'
+
+export default function App() {
+  return (
+    <AppContainer>
+      <Navigator />
+    </AppContainer>
+  )
+}
